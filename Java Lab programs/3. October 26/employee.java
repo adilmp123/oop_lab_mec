b9ff082cd667817f6a142447e3a958acf0d9aa13
adilmp123 <87594500+adilmp123@users.noreply.github.com>
2022-11-02 15:55:10 +0530

    Update employee.java

--- Java Lab programs/3. October 26/employee.java
+++ Java Lab programs/3. October 26/employee.java
@@ -13,7 +13,7 @@ class employee
         System.out.print(salary);
     }
 
-    void readEmployee(employee p)
+    void readEmployee(Officer p)
     {
         System.out.print("Enter Name: ");
         p.name=read.next();
@@ -25,16 +25,44 @@ class employee
         p.address=read.next();
         System.out.print("Enter Salary: ");
         p.salary=read.nextInt();
+        System.out.print("Enter specialisation: ");
+        p.specialization=read.next();
+        System.out.print("\n\n");
+    }
+    void readEmployee(Manager p)
+    {
+        System.out.print("Enter Name: ");
+        p.name=read.next();
+        System.out.print("Enter Age: ");
+        p.age=read.nextInt();
+        System.out.print("Enter Phone Number: ");
+        p.ph=read.next();
+        System.out.print("Enter Address: ");
+        p.address=read.next();
+        System.out.print("Enter Salary: ");
+        p.salary=read.nextInt();
+        System.out.print("Enter department: ");
+        p.department=read.next();
         System.out.print("\n\n");
     }
 
-    static void printEmployee(employee p)
+    static void printEmployee(Officer p)
     {
         System.out.println("Name: "+p.name);
         System.out.println("Age: "+p.age);
         System.out.println("Ph: "+p.ph);
         System.out.println("Address: "+p.address);
-        System.out.println("Salary: "+p.salary + "\n\n\n");
+        System.out.println("Salary: "+p.salary);
+        System.out.println("Specialisation: "+p.specialization+ "\n\n\n");
+    }
+    static void printEmployee(Manager p)
+    {
+        System.out.println("Name: "+p.name);
+        System.out.println("Age: "+p.age);
+        System.out.println("Ph: "+p.ph);
+        System.out.println("Address: "+p.address);
+        System.out.println("Salary: "+p.salary);
+        System.out.println("department: "+p.department + "\n\n\n");
     }
 
     public static void main(String args[])
