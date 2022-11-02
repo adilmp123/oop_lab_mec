aa71cd6adef7e90475541827019f3c5efe2162c3
adilmp123 <87594500+adilmp123@users.noreply.github.com>
2022-11-02 15:55:28 +0530

    Update garbage.java

--- Java Lab programs/3. October 26/garbage.java
+++ Java Lab programs/3. October 26/garbage.java
@@ -3,9 +3,10 @@ class garbage
     public static void main(String[] args)
     {
         garbage g = new garbage();
-        System.out.println(Runtime.getRuntime().freeMemory());
+        System.out.println("free memmory before = "+Runtime.getRuntime().freeMemory());
         g=null;
         System.gc();
-        System.out.println(Runtime.getRuntime().freeMemory());
+        System.out.println("free memmory after = "+Runtime.getRuntime().freeMemory());
+        
     }
 }
